54ce0561dda0a4443fd8471f1216038445019d71
Charly <sleepycharlyy@hotmail.com>
2020-07-10 21:06:55 +0200

    small changes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# LCG Example Python
-a linear congruential generator example in python 
+# LCG Example Python:
+
+a linear congruential generator example in python. seed is generated with the current time
+
+## Video about this Topic:
+
+Work in progress...
+
+## Functions:
+
+- randomInt() - generates random Integer between '0' and '2^48'
+- randomFloat() - generates random Float between '0' and '1'
+- randomIntRange(min, max) - generates random Integer between variables 'min' and 'max'
+- randomFloatRange(min, max) - generates random Float between variables 'min' and 'max'
+- randomBool(Chance) - generates random Boolean with the chance of getting true being 'chance' (Default: 0.5)
--- linear_congruential_generator.py
+++ linear_congruential_generator.py
@@ -1,60 +1,60 @@
 # imports
-import time
-import math
+import time;
+import math;
 
 # parameters
-seed = int(round(time.time() * 1000))
-multiplier = 25214903917
-increment = 11
-modulus = pow(2, 48)
+seed = int(round(time.time() * 1000));
+multiplier = 25214903917;
+increment = 11;
+modulus = pow(2, 48);
 
 # a float is a number with decimals
 # an integer is a whole number
 # a boolean is true or false
 
 def randomInt(): # range [0 to 2^48]
-    global seed
-    seed = (seed * multiplier + increment) % modulus
-    return seed
+    global seed;
+    seed = (seed * multiplier + increment) % modulus;
+    return seed;
 
 def randomFloat(): # range [0 to 1]
-    return (randomInt() / modulus)
+    return (randomInt() / modulus);
 
 def randomIntRange(min, max): # custom range [minimum, maximum]
-    return (math.floor(randomFloatRange(min, max)))
+    return (math.floor(randomFloatRange(min, max)));
 
 def randomFloatRange(min, max): # custom range [minimum, maximum]
-    return (min + randomFloat() * (max - min))
+    return (min + randomFloat() * (max - min));
 
 def randomBool(chance): # chance of getting true (example: randomBool(20) has a 20% chance of returning true)
     if(chance == 0):
-        chance = 0.5
-    return (randomFloat() < chance)
+        chance = 0.5;
+    return (randomFloat() < chance);
 
 
 # execute functions and print results
-print("Sequence of Random Integers between 0 and the Modulus")
-print(randomInt())
-print(randomInt())
-print(randomInt())
-print(randomInt())
-print("\nSequence of Random Floats between 0 and 1")
-print(randomFloat())
-print(randomFloat())
-print(randomFloat())
-print(randomFloat())
-print("\nSequence of Random Integers between 100 and 250")
-print(randomIntRange(100, 250))
-print(randomIntRange(100, 250))
-print(randomIntRange(100, 250))
-print(randomIntRange(100, 250))
-print("\nSequence of Random Floats between 60 and 70")
-print(randomFloatRange(60, 70))
-print(randomFloatRange(60, 70))
-print(randomFloatRange(60, 70))
-print(randomFloatRange(60, 70))
-print("\nSequence of Random Booleans with a chance of 30% of getting true")
-print(randomBool(0.3))
-print(randomBool(0.3))
-print(randomBool(0.3))
-print(randomBool(0.3))
+print("Sequence of Random Integers between 0 and the Modulus");
+print(randomInt());
+print(randomInt());
+print(randomInt());
+print(randomInt());
+print("\nSequence of Random Floats between 0 and 1");
+print(randomFloat());
+print(randomFloat());
+print(randomFloat());
+print(randomFloat());
+print("\nSequence of Random Integers between 100 and 250");
+print(randomIntRange(100, 250));
+print(randomIntRange(100, 250));
+print(randomIntRange(100, 250));
+print(randomIntRange(100, 250));
+print("\nSequence of Random Floats between 60 and 70");
+print(randomFloatRange(60, 70));
+print(randomFloatRange(60, 70));
+print(randomFloatRange(60, 70));
+print(randomFloatRange(60, 70));
+print("\nSequence of Random Booleans with a chance of 30% of getting true");
+print(randomBool(0.3));
+print(randomBool(0.3));
+print(randomBool(0.3));
+print(randomBool(0.3));
